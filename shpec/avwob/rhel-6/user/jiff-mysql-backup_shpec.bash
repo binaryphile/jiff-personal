1ded9554158cec9e5d9df783688ad37f23c135dd
Ted Lilley <ted.lilley@accelerated.com>
2017-10-04 14:43:29 -0400

    test filename and dry-run

--- shpec/avwob/rhel-6/user/jiff-mysql-backup_shpec.bash
+++ shpec/avwob/rhel-6/user/jiff-mysql-backup_shpec.bash
@@ -42,6 +42,17 @@ describe mback_main
     assert equal '0 0' "$? $rc"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
+
+  it "outputs its backup command if invoked with the dry_run flag"; ( _shpec_failures=0
+    result=$(mback_main dry_run_flag=1 full sample)
+    rc=$?
+    get <<'    EOS'
+      mysqldump --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf --all-databases --single-transaction
+    EOS
+    [[ $result == "$__" ]]
+    assert equal '0 0' "$? $rc"
+    return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
+  end
 end
 
 describe mbackup
@@ -66,6 +77,23 @@ describe mbackup
       --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf --master-data --single-transaction avwob_production
     EOS
     assert equal "$__" "$(mbackup master sample)"
+    return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
+  end
+
+  it "names the file after the type and host"; ( _shpec_failures=0
+    stub_command mysqldump
+    stub_command gzip
+    stub_command redirect_to  'echo "$@"'
+    stub_command date 'echo 1-2-3'
+    assert equal /opt/app/avdata/backup/sample-master-1-2-3.sql.gz "$(mbackup master sample)"
+    return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
+  end
+
+  it "outputs the command with the dry-run flag"; ( _shpec_failures=0
+    get <<'    EOS'
+      mysqldump --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf --master-data --single-transaction avwob_production
+    EOS
+    assert equal "$__" "$(mbackup master sample dry_run=1)"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 end
