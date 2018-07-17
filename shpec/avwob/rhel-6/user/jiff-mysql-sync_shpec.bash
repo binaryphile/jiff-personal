29f0d104069ee0a72f2119820feff2e48e582ba3
Ted Lilley <ted.lilley@digi.com>
2018-07-17 12:40:15 -0400

    update mysql sync

--- shpec/avwob/rhel-6/user/jiff-mysql-sync_shpec.bash
+++ shpec/avwob/rhel-6/user/jiff-mysql-sync_shpec.bash
@@ -2,36 +2,36 @@ source concorde.bash
 
 $(require_relative ../../../../context/avwob/rhel-6/user/jiff-mysql-sync)
 
-describe msync_main
+describe main
   it "reports usage if not provided a slave argument"; ( _shpec_failures=0
-    result=$(msync_main '')
+    result=$(main '')
     rc=$?
     [[ $result == $'\nUsage:'* ]]
     assert equal '0 0' "$? $rc"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 
-  it "calls msync when given --dry-run"; ( _shpec_failures=0
-    stub_command msync 'echo "$@"'
+  it "calls sync when given --dry-run"; ( _shpec_failures=0
+    stub_command sync 'echo "$@"'
     dry_run_flag=1
     stuff dry_run_flag into ''
-    assert equal 'sample __' "$(msync_main __ sample)"
+    assert equal 'sample __' "$(main __ sample)"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 
-  it "calls msync when given a master"; ( _shpec_failures=0
-    stub_command msync 'echo "$@"'
-    assert equal 'sample __' "$(msync_main '' sample example)"
+  it "calls sync when given a master"; ( _shpec_failures=0
+    stub_command sync 'echo "$@"'
+    assert equal 'sample __' "$(main '' sample example)"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 end
 
-describe msync
+describe sync
   it "reports the command when given dry_run_flag with only slave"; ( _shpec_failures=0
     get <<'    EOS'
       mysqldump --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf --single-transaction --master-data avwob_production | mysql --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf -h sample avwob_production
     EOS
-    assert equal "$__" "$(msync sample dry_run_flag=1)"
+    assert equal "$__" "$(sync sample dry_run_flag=1)"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 
@@ -39,7 +39,7 @@ describe msync
     get <<'    EOS'
       mysqldump --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf --single-transaction --master-data -h example avwob_production | mysql --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf -h sample avwob_production
     EOS
-    assert equal "$__" "$(msync sample dry_run_flag=1 master=example)"
+    assert equal "$__" "$(sync sample dry_run_flag=1 master=example)"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 
@@ -49,7 +49,7 @@ describe msync
     get <<'    EOS'
       --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf --single-transaction --master-data avwob_production
     EOS
-    assert equal "$__" "$(msync sample '')"
+    assert equal "$__" "$(sync sample '')"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 
@@ -59,14 +59,14 @@ describe msync
     get <<'    EOS'
       --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf -h sample avwob_production
     EOS
-    assert equal "$__" "$(msync sample '')"
+    assert equal "$__" "$(sync sample '')"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 
   it "calls mysqldump with slave and master arguments"; ( _shpec_failures=0
     stub_command mysqldump  'echo "$@"'
     stub_command mysql      cat
-    result=$(msync sample master=example)
+    result=$(sync sample master=example)
     get <<'    EOS'
       --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf --single-transaction --master-data -h example avwob_production
     EOS
@@ -80,7 +80,7 @@ describe msync
     get <<'    EOS'
       --defaults-extra-file=/opt/app/avwobt4/etc/mysql/backup.cnf -h sample avwob_production
     EOS
-    assert equal "$__" "$(msync sample '')"
+    assert equal "$__" "$(sync sample '')"
     return "$_shpec_failures" ); : $(( _shpec_failures += $? ))
   end
 end
